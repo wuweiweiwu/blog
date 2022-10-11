5cf25d7d3cb8bee3d2a55e1787036a180c035992
wuweiweiwu <hello@weiweiwu.me>
2022-10-10 22:55:57 -0700

    some base url shenanigans

--- _data/env.js
+++ _data/env.js
@@ -1,5 +1,11 @@
+const isDev = /serve|watch/.test(process.argv.join());
+
+const baseUrl = isDev ? `localhost:8080` : `https://weiweiwu.me`;
+
 module.exports = {
   tracking: {
     gtag: "GTM-5JNK4F8",
   },
+  baseUrl,
+  isDev,
 };
